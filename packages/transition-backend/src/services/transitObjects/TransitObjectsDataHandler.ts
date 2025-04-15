4350a1c904f66a7e207a78917c074b315275013e
abdel <abdel-madjid.santanna@polymtl.ca>
2025-04-14 20:09:35 -0400

    updated reference to saveAll in TransitObjectsDataHandler

--- packages/transition-backend/src/services/transitObjects/TransitObjectsDataHandler.ts
+++ packages/transition-backend/src/services/transitObjects/TransitObjectsDataHandler.ts
@@ -460,7 +460,7 @@ function createDataHandlers(): Record<string, TransitObjectDataHandler> {
 async function updateSchedulesBatch(attributes: ScheduleAttributes[] ): Promise<Status.Status<number[]>> {
     try {
         console.log('updateSchedulesBatch appelé avec');
-        const updatedSchedules = await schedulesDbQueries.batchUpdate(attributes);
+        const updatedSchedules = await schedulesDbQueries.saveAll(attributes);
         return Status.createOk(updatedSchedules);
     } catch (error) {
         console.error('Error batch updating schedules: ', error);
